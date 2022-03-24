96875b85cbd8b4b33b39ee7541be87081b2f0559
LilianBarrosLima <lilian.barros08@yahoo.com.br>
2022-03-24 13:56:46 -0300

    Implementing User sign up

---
+++ src/main/java/com/lilianlima/photoapp/api/users/service/UsersService.java
@@ -0,0 +1,7 @@
+package com.lilianlima.photoapp.api.users.service;
+
+import com.lilianlima.photoapp.api.users.shared.UserDto;
+
+public interface UsersService {
+	UserDto createUser (UserDto userDetails);
+}
